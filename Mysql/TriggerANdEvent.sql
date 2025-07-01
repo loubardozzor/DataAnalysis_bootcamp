464376e30e65d610bfa22b9cbcf8b67b541484d3
hyacinthe <hyacinthetchoula@gmail.com>
2025-07-01 15:50:36 +0200

    trigger and event

---
+++ Mysql/TriggerANdEvent.sql
@@ -0,0 +1,46 @@
+-- TRIGGER AND EVENTS
+
+
+-- TRIGGER
+SELECT *
+FROM employee_salary;
+
+
+DELIMITER $$
+CREATE TRIGGER employee_insert
+	AFTER INSERT ON employee_salary
+    FOR EACH ROW
+BEGIN
+	INSERT INTO employee_demographics (employee_id, first_name, last_name)
+    VALUES (NEW.employee_id, NEW.first_name, NEW.last_name);
+END $$
+DELIMITER ;
+
+
+select *
+from employee_salary;
+
+select *
+from employee_demographics;
+
+INSERT INTO employee_salary (employee_id, first_name, last_name, occupation, salary, dept_id)
+VALUES(13, 'Jean-Ralphio', 'Saperstein','Exnterntaiment 720 CEO', 1000000, NULL);
+
+
+-- EVENTS
+
+select *
+from employee_demographics;
+
+DELIMITER $$
+CREATE EVENT delete_retirees
+ON SCHEDULE EVERY 30 SECOND 
+DO 
+BEGIN
+	DELETE
+    FROM employee_demographics
+    WHERE age >= 60;
+END $$
+DELIMITER ;
+
+SHOW VARIABLES LIKE 'event%';
